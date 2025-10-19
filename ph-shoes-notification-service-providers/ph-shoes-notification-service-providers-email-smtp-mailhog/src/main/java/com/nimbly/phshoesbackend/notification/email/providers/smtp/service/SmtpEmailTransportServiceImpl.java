7db0828dcc3e1d1e2e7d7489cc82a6fcc28f6754
Altheo Saquilayan <altheo.saquilayan@exist.com>
2025-10-19 19:06:12 +0800

    Update

--- ph-shoes-notification-service-providers/ph-shoes-notification-service-providers-email-smtp-mailhog/src/main/java/com/nimbly/phshoesbackend/notification/email/providers/smtp/service/SmtpEmailTransportServiceImpl.java
+++ ph-shoes-notification-service-providers/ph-shoes-notification-service-providers-email-smtp-mailhog/src/main/java/com/nimbly/phshoesbackend/notification/email/providers/smtp/service/SmtpEmailTransportServiceImpl.java
@@ -6,6 +6,7 @@ import com.nimbly.phshoesbackend.notification.core.model.dto.SendResult;
 import com.nimbly.phshoesbackend.notification.core.service.EmailTransportService;
 import jakarta.mail.Session;
 import jakarta.mail.internet.MimeMessage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,7 @@ import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 
+@Slf4j
 @Service
 public class SmtpEmailTransportServiceImpl implements EmailTransportService {
 
@@ -21,19 +23,20 @@ public class SmtpEmailTransportServiceImpl implements EmailTransportService {
 
     public SmtpEmailTransportServiceImpl(JavaMailSender mailSender) {
         this.mailSender = mailSender;
+        if (mailSender instanceof JavaMailSenderImpl impl) {
+            log.info("[SMTP] Using host={} port={}", impl.getHost(), impl.getPort());
+        } else {
+            log.info("[SMTP] Using JavaMailSender type={}", mailSender.getClass().getName());
+        }
     }
 
     @Override
     public SendResult send(ComposedEmail email) throws NotificationSendException {
         try {
-            Session session = (mailSender instanceof JavaMailSenderImpl impl && impl.getSession() != null)
-                    ? impl.getSession()
-                    : Session.getInstance(System.getProperties());
-
-            MimeMessage mime = new MimeMessage(
-                    session,
-                    new ByteArrayInputStream(email.getRawMime().getBytes(StandardCharsets.UTF_8))
-            );
+            ByteArrayInputStream in = new ByteArrayInputStream(email.getRawMime().getBytes(StandardCharsets.UTF_8));
+            MimeMessage mime = (mailSender instanceof JavaMailSenderImpl jms)
+                    ? jms.createMimeMessage(in)
+                    : mailSender.createMimeMessage(in);
 
             mailSender.send(mime);
 
