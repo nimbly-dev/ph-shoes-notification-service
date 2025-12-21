68d40ef0d6a3379f20cd969ae77b8d2b6d539afe
Altheo Saquilayan <altheo.saquilayan@exist.com>
2025-12-21 22:28:01 +0800

    PHSHOE-104: User Accounts is throwing Internal Server Error when registering

--- ph-shoes-notification-service-core/src/main/java/com/nimbly/phshoesbackend/notification/core/exception/NotificationSendException.java
+++ ph-shoes-notification-service-core/src/main/java/com/nimbly/phshoesbackend/notification/core/exception/NotificationSendException.java
@@ -1,7 +1,11 @@
 package com.nimbly.phshoesbackend.notification.core.exception;
 
 public class NotificationSendException extends RuntimeException {
-    public NotificationSendException(String message, Exception e) {
+    public NotificationSendException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotificationSendException(String message) {
         super(message);
     }
 }
