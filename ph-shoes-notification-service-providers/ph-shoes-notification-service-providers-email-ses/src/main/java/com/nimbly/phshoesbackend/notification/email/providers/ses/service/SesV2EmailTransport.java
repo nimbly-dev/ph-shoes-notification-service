82aa02a7014e41497292b27a099e6d91342ea591
Altheo Saquilayan <altheo.saquilayan@exist.com>
2025-12-26 11:36:02 +0800

    PHSHOE-108: Alerts Scheduler is not emailing alert notification

--- ph-shoes-notification-service-providers/ph-shoes-notification-service-providers-email-ses/src/main/java/com/nimbly/phshoesbackend/notification/email/providers/ses/service/SesV2EmailTransport.java
+++ ph-shoes-notification-service-providers/ph-shoes-notification-service-providers-email-ses/src/main/java/com/nimbly/phshoesbackend/notification/email/providers/ses/service/SesV2EmailTransport.java
@@ -7,6 +7,7 @@ import com.nimbly.phshoesbackend.notification.core.service.EmailTransportService
 import com.nimbly.phshoesbackend.notification.email.providers.ses.config.NotificationSesProps;
 import com.nimbly.phshoesbackend.notification.email.providers.ses.util.TemplateJsonSerializer;
 import org.springframework.stereotype.Service;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import software.amazon.awssdk.core.SdkBytes;
 import software.amazon.awssdk.services.sesv2.SesV2Client;
 import software.amazon.awssdk.services.sesv2.model.*;
@@ -16,6 +17,7 @@ import java.util.List;
 import java.util.Map;
 
 @Service
+@ConditionalOnExpression("'${notification.transport:}'=='ses' or '${notification.transport:}'=='sesv2'")
 public class SesV2EmailTransport implements EmailTransportService {
 
     private final SesV2Client sesClient;
